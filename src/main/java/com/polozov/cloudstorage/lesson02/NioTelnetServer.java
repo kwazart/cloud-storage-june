e4dfd95b3462d6f235aec45dc529c41539559be4
Nezhdanov Sergei <tsebom@yandex.ru>
2021-06-28 19:48:23 +0300

    homework2.2

--- src/main/java/com/polozov/cloudstorage/lesson02/NioTelnetServer.java
+++ src/main/java/com/polozov/cloudstorage/lesson02/NioTelnetServer.java
@@ -10,14 +10,21 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
 
 public class NioTelnetServer {
-    private static final String LS_COMMAND = "\tls     view all files from current directory";
-    private static final String MKDIR_COMMAND = "\tmkdir  view all files from current directory";
+    private static final String LS_COMMAND = "ls          view all files from current directory\n\r";
+    private static final String MKDIR_COMMAND = "mkdir       view all files from current directory\n\r";
+    private static final String TOUCH_COMMAND = "touch       create a file\n\r";
+    private static final String CD_COMMAND = "cd          change directory\n\r";
+    private static final String RM_COMMAND = "rm          remove file or directory\n\r";
+    private static final String COPY_COMMAND = "copy        copy file or directory\n\r";
+    private static final String CAT_COMMAND = "cat         output file data\n\r";
+    private static final String CHANGENICK_COMMAND = "changenick  change nickname\n\r";
+
+    private Path currentPath = Paths.get("server");
 
     private final ByteBuffer buffer = ByteBuffer.allocate(512);
 
@@ -52,10 +59,8 @@ public class NioTelnetServer {
         channel.configureBlocking(false);
         System.out.println("Client connected. IP:" + channel.getRemoteAddress());
         channel.register(selector, SelectionKey.OP_READ, "skjghksdhg");
-        channel.write(ByteBuffer.wrap("Hello user!\n".getBytes(StandardCharsets.UTF_8)));
-        channel.write(ByteBuffer.wrap("Enter --help for support info".getBytes(StandardCharsets.UTF_8)));
+        channel.write(ByteBuffer.wrap("Hello user!\n\r".getBytes(StandardCharsets.UTF_8)));
     }
-
 
     private void handleRead(SelectionKey key, Selector selector) throws IOException {
         SocketChannel channel = (SocketChannel) key.channel();
@@ -91,13 +96,76 @@ public class NioTelnetServer {
             String command = sb.toString()
                     .replace("\n", "")
                     .replace("\r", "");
-            if ("--help".equals(command)) {
+
+            if (command.contains(" ")) {
+                String param = splitCommand(command)[1];
+                Path path = currentPath.resolve(param);
+                if (command.startsWith("mkdir")) {
+                    Files.createDirectories(path);
+                } else if (command.startsWith("touch")) {
+                    Files.createFile(path);
+                } else if (command.startsWith("cd")) {
+                    goToAdBack(path, param, selector, client);
+                } else if (command.startsWith("rm")) {
+                    removeFileOrDir(path);
+                } else if (command.startsWith("copy")) {
+                    copyFile(command);
+                }
+            } else if ("--help".equals(command)) {
                 sendMessage(LS_COMMAND, selector, client);
                 sendMessage(MKDIR_COMMAND, selector, client);
+                sendMessage(TOUCH_COMMAND, selector, client);
+                sendMessage(CD_COMMAND, selector, client);
+                sendMessage(COPY_COMMAND, selector, client);
+                sendMessage(CAT_COMMAND, selector, client);
+                sendMessage(RM_COMMAND, selector, client);
+                sendMessage(CHANGENICK_COMMAND, selector, client);
             } else if ("ls".equals(command)) {
-                sendMessage(getFilesList().concat("\n"), selector, client);
+                sendMessage(getFilesList().concat("\n\r"), selector, client);
+            } else {
+                sendMessage("Enter --help for support info\n\r", selector,client);
             }
         }
+        sendMessage(currentPath.toString().concat(": "), selector, client);
+    }
+
+    private void copyFile(String command) throws IOException {
+        String[] paths = splitCommand(command);
+        if (Files.isDirectory(currentPath.resolve(paths[1]))) {
+            walkTreeForCopy(currentPath.resolve(Paths.get(paths[1])), currentPath.resolve(Paths.get(paths[2])));
+        } else {
+            Files.copy(currentPath.resolve(paths[1]).normalize(), currentPath.resolve(paths[2]).normalize());
+        }
+    }
+
+    private void walkTreeForCopy (Path pathFrom, Path pathTo) throws IOException {
+        List<File> list = new ArrayList<>();
+        String from = pathFrom.toString();
+        String to = pathTo.toString();
+
+        Files.walkFileTree(pathFrom, new SimpleFileVisitor<Path>() {
+
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                File[] files = new File(dir.toAbsolutePath().toString()).listFiles();
+                for (File f: files) {
+                    list.add(f);
+                }
+
+                if (list.size() != 0) {
+                    for (File file : list) {
+                        String f = file.toString().replace(from, to);
+                        if (file.isDirectory()) {
+                            Files.createDirectory(Paths.get(f));
+                        } else {
+                            Files.copy(file.toPath(), Paths.get(f));
+                        }
+                    }
+                }
+                list.clear();
+                return FileVisitResult.CONTINUE;
+            }
+        });
     }
 
     private void sendMessage(String message, Selector selector, SocketAddress client) throws IOException {
@@ -110,9 +178,64 @@ public class NioTelnetServer {
         }
     }
 
+    private String[] splitCommand(String command) {
+        return command.split(" ");
+    }
+
     private String getFilesList() {
-        String[] servers = new File("server").list();
-        return String.join(" ", servers);
+        String[] servers = new File(currentPath.toAbsolutePath().toString()).list();
+        return String.join("\n\r" + "", servers);
+    }
+
+    private void goToAdBack(Path path, String param, Selector selector, SocketAddress client) throws IOException {
+        if (param.equals("~")) {
+            currentPath = Paths.get("server");
+        } else if (param.equals("..") && !(currentPath.toString()).equals("server")) {
+            currentPath = currentPath.getParent();
+        } else if (Files.isDirectory(path)) {
+            currentPath = path;
+        } else {
+            sendMessage("Directory like this is not exist", selector, client);
+        }
+    }
+
+    private void removeFileOrDir (Path path) throws IOException {
+        if (Files.isDirectory(path)) {
+            List<File> list = walkTree(path);
+            while (!list.isEmpty()) {
+                Iterator<File> iterator = list.iterator();
+                while (iterator.hasNext()) {
+                    File file = iterator.next();
+                    if (!file.isDirectory() || (file.isDirectory() && isDirectoryEmpty(file))) {
+                        file.delete();
+                        iterator.remove();
+                        list.remove(file);
+                    }
+                }
+            }
+        }
+        Files.delete(path);
+    }
+
+    private List<File> walkTree (Path path) throws IOException {
+        List<File> list = new ArrayList<>();
+
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                File[] files = new File(dir.toAbsolutePath().toString()).listFiles();
+                for (File f: files) {
+                    list.add(f);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return list;
+    }
+
+    private boolean isDirectoryEmpty(File directory) {
+        String[] files = directory.list();
+        return files.length == 0;
     }
 
     public static void main(String[] args) throws Exception {
